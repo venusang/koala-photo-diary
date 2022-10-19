81f42f39c336c47e92d09586be0022f396f683f2
Venus Ang <venus@users.noreply.github.com>
2022-10-18 18:19:05 -0700

    adding a lot of files - kept index.js as is

---
+++ bin.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+const open = require('open')
+const { exec, spawn } = require('child_process')
+exec('npx webpack --mode="development"', () => {
+    spawn('npx', [ 'webpack', '--watch', '--mode="development"' ], { stdio: 'inherit' });
+    open('./public/index.html')
+    console.log('Watching for changes in "./src"')
+})
+
+spawn('node', ['script.js'], {
+    env: {
+        NODE_ENV: 'production',
+        PATH: process.env.PATH
+    }
+})
